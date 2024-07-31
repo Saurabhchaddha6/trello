ee18d4d35bb929f9ef7a658b041b5d105508bb33
saurabhchaddha6 <saurabhchaddha6@gmail.com>
2024-07-31 16:09:58 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+"use client"
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const {data: session} = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session || !session.user) {return;}
+    else{
+      router.replace('/dashboard');
+    }
+  }, [router, session]);
+
+
   return (
     <div>
       
