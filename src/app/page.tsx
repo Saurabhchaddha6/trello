70b52ef0e17ac78bd7e934fd6aa2f698c30f4302
saurabhchaddha6 <saurabhchaddha6@gmail.com>
2024-07-31 16:15:40 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,22 @@
-"use client"
+"use client";
+
 import { useSession } from "next-auth/react";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
-  const {data: session} = useSession();
+  const { data: session } = useSession();
   const router = useRouter();
 
   useEffect(() => {
-    if (!session || !session.user) {return;}
-    else{
-      router.replace('/dashboard');
+    if (session && session.user) {
+      // User is authenticated, redirect to dashboard
+      router.push("/dashboard");
+    } else {
+      // User is not authenticated, redirect to sign-up page
+      router.push("/signup");
     }
-  }, [router, session]);
+  }, [session, router]);
 
-
-  return (
-    <div>
-      
-    </div>
-  );
+  return null; // No need to render anything as this component handles redirection
 }
